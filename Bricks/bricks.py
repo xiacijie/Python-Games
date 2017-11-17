5f37b875d874e7b79a8b7ff4d8702825fc17f486
xiacijie <cijie@ualberta.ca>
2017-11-16 22:50:37 -0700

    Struct finished

---
+++ Bricks/bricks.py
@@ -0,0 +1,52 @@
+import pygame
+pygame.init()
+pygame.font.init()
+
+
+#RBG color setting
+black = (0,0,0)
+white = (255,255,255)
+blue = (25,25,112)
+green = (0,100,0)
+red = (255,0,0)
+grey = (205,201,201)
+yellow = (255,255,0)
+purple = (160,32,240)
+#size setting 
+screen_size = 600
+seg_size = 15
+speed = 0.36
+FPS = 10
+#initialize screen
+screen = pygame.display.set_mode((screen_size,screen_size))
+
+clock = pygame.time.Clock()
+back_groung_pic = pygame.image.load("grass.jpg")
+pygame.mixer.music.load("back1.mp3")
+#Connection to the database
+connection = None
+cursor = None
+def main():
+
+class Game:
+	def __init__(self):
+		pass
+
+	def play(self):
+		pass
+
+
+class Brick: # single brick
+	def __init__(self):
+		pass
+
+class Ball: # single ball
+	def __init__(self):
+		pass
+
+class Board:
+	def __init__(self):
+		pass
+
+		
+main()
