695e42db2072f86fa04f4d5659aca48215738a73
xiacijie <cijie@ualberta.ca>
2017-11-17 01:21:10 -0700

    bug in board

--- Bricks/bricks.py
+++ Bricks/bricks.py
@@ -16,10 +16,10 @@ purple = (160,32,240)
 screen_size = 600
 seg_size = 15
 speed = 0.36
-FPS = 15
+FPS = 180
 #initialize screen
 screen = pygame.display.set_mode((screen_size,screen_size))
-
+pygame.key.set_repeat(10,5)
 clock = pygame.time.Clock()
 #back_groung_pic = pygame.image.load("grass.jpg")
 #pygame.mixer.music.load("back1.mp3")
@@ -36,15 +36,18 @@ class Game:
 		self.game_play  = True
 		self.board = Board()
 
+
 	def play(self):
 		while self.game_play:
 			clock.tick(FPS)
 			self.ball.draw()
 			self.ball.move()
-			self.ball.bounce()
+			self.ball.bounce_edge()
+			self.ball_bounce_board()
 			self.board.draw()
 			self.get_key()
-			
+			self.ball_bounce_board()
+			self.board_is_moving = False
 			pygame.display.update()
 			screen.fill(black)
 
@@ -52,11 +55,20 @@ class Game:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
-			elif event.type == pygame.KEYDOWN:
+			if event.type == pygame.KEYDOWN:
+
 				if event.key == pygame.K_LEFT: 
 					self.board.move("left")
+					#self.ball_bounce_board("left")
 				elif event.key == pygame.K_RIGHT:
-					self.board.move("right")			
+					self.board.move("right")
+					#self.ball_bounce_board("right")
+
+
+	def ball_bounce_board(self):
+		if self.board.rect.collidepoint(self.ball.x_coord,self.ball.y_coord):
+			self.ball.bounce_board()
+
 
 
 class Brick: # single brick
@@ -68,7 +80,7 @@ class Ball: # single ball
 		self.x_coord = 300
 		self.y_coord = 550
 		self.x_speed = 0
-		self.y_speed = -5
+		self.y_speed = -1
 		self.radius = 5
 
 	def draw(self):
@@ -78,16 +90,25 @@ class Ball: # single ball
 		self.x_coord += self.x_speed
 		self.y_coord += self.y_speed
 
-	def bounce(self):
+	def bounce_edge(self):
 		if self.x_coord == 0+self.radius or self.x_coord == screen_size - self.radius:
 			self.x_speed = -self.x_speed
 		elif self.y_coord == 0+self.radius:
 			self.y_speed = -self.y_speed
 
+	def bounce_board(self):
+		
+		self.y_speed = -self.y_speed
+		#if direction == "left":
+			#self.x_speed = -10
+		#elif direction == "right":
+			#self.x_speed = 10
+
 class Board:
 	def __init__(self):
 		width = 30
-		self.rect = pygame.Rect(screen_size/2-0.5*width,560,10)
+		left = screen_size/2-0.5*width
+		self.rect = pygame.Rect(left,560,70,5)
 
 	def draw(self):
 		pygame.draw.rect(screen,white,self.rect,0)
