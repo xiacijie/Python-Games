98d15251e609027684d47b10fa1e2d83d4903b90
xiacijie <cijie@ualberta.ca>
2017-11-20 21:56:01 -0700

    start

--- Race car/car.py
+++ Race car/car.py
@@ -42,10 +42,12 @@ def main():
 class Game:
 
 	def __init__(self):
-		pass
+		self.border = Border()
+
 
 	def play(self):
 		while True:
+			self.border.draw()
 			self.get_key()
 
 	def get_key(self): #Get the user input through the key board
@@ -53,9 +55,46 @@ class Game:
 			if event.type == pygame.QUIT:
 				sys.exit()
 
+class Segment:
+	def __init__(self,left,top,width,height):
+		self.rect = pygame.Rect(left,top,width,height)
+
 class Border:
 	def __init__(self):
-		pass
+		self.left_border = []
+		self.right_border = []
+
+	def create_border(self):
+		height = 30
+		white_space = 30
+		top = 0
+		width = 10
+		# create left border
+		for i in range(screen_size//(height+white_space)):
+			segment = Segment(screen_size//4,top,width,height)
+			self.left_border.append(segment)
+			top += white_space
+
+		#create right border
+		top = 0
+		for i in range(screen_size//(height+white_space)):
+			segment = Segment(3*(screen_size//4),top,width,height)
+			self.right_border.append(segment)
+			top += white_space
+		
+	def draw(self):
+		for seg in self.left_border:
+			pygame.draw.rect(screen,white,seg.rect,0)
+		for seg in self.right_border:
+			pygame.draw.rect(screen,white,seg.rect,0)
+
+
+
+
+
+class Central_line:
+	def __init__(self):
+
 
 
 class Your_car:
